e2f955fa514a2ea8536e5eaf268b031bfadbe88a
rono23 <rono23@gmail.com>
2020-09-17 09:25:16 +0900

    Update Nginx configuration

--- recipes/nginx.sh
+++ recipes/nginx.sh
@@ -4,5 +4,6 @@ else
   sunzi.install nginx
   mv files/nginx.conf /etc/nginx/sites-available/<%= @attributes.app_name %>
   ln -s /etc/nginx/sites-available/<%= @attributes.app_name %> /etc/nginx/sites-enabled/<%= @attributes.app_name %>
+  rm /etc/nginx/sites-enabled/default
   service nginx restart
 fi
