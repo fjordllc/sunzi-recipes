e4e8336922d95c2da6df2383134fb0ca733a3651
rono23 <rono23@gmail.com>
2020-09-17 09:25:10 +0900

    Remove wheezy setups

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ set -e
 source recipes/sunzi.sh
 export DEBIAN_FRONTEND=noninteractive
 source recipes/ssh.sh
-source recipes/apt-jp.sh
-source recipes/backports-wheezy.sh
 sunzi.mute 'apt-get update'
 sunzi.mute 'apt-get -y upgrade'
 source recipes/locale-jp.sh
--- recipes/apt-jp.sh
+++
@@ -1,5 +0,0 @@
-cat <<EOF > /etc/apt/sources.list
-deb http://ftp.jp.debian.org/debian/ wheezy main contrib non-free
-deb http://security.debian.org/ wheezy/updates main contrib non-free
-deb http://ftp.jp.debian.org/debian/ wheezy-updates main contrib non-free
-EOF
